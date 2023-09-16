89019c20b237cf9cbb5862b6c937407a109fca0a
Rahul Gupta <115313128+RahulGupta403@users.noreply.github.com>
2023-09-17 01:34:56 +0530

    Created Print_First_N_Fibonacci_Number.cpp

---
+++ Print_First_N_Fibonacci_Number.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+
+class Solution
+{
+    public:
+    //Function to return list containing first n fibonacci numbers.
+    vector<long long> printFibb(int n) 
+    {
+        vector<long long>ans;
+        long long int prev2=1;
+        ans.push_back(prev2);
+        if(n==1){
+            return ans;
+        }
+        long long int prev=1;
+        ans.push_back(prev);
+        if(n==2){
+            return ans;
+        }
+        for(int i=3;i<=n;i++){
+            long long int sum=prev2+prev;
+            ans.push_back(sum);
+            prev2=prev;
+            prev=sum;
+        }
+    return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking number of elements
+        int n;
+        cin>>n;
+        Solution obj;
+        //calling function printFibb()
+        vector<long long> ans = obj.printFibb(n);
+        
+        //printing the elements of vector
+        for(long long i:ans)cout<<i<<' ';
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
