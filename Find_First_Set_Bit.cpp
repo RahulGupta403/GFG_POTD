65b2db72316e751fe2c17d901ee5f4da9dd3fdfc
Rahul Gupta <115313128+RahulGupta403@users.noreply.github.com>
2023-09-19 00:21:32 +0530

    Created Find_First_Set_Bit.cpp

---
+++ Find_First_Set_Bit.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+        if(n==0 or n==1){
+            return n;
+        }
+        unsigned int count=1;
+        while(n){
+            if((n&1)==0){
+                count++;
+            }else{
+                break;
+            }
+            n>>=1;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
+
+// } Driver Code Ends
