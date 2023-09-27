189656429d90b843cab00d46b0bd29391d8e302c
Rahul Gupta <115313128+RahulGupta403@users.noreply.github.com>
2023-09-28 01:29:12 +0530

    Created Wave_Array.cpp

---
+++ Wave_Array.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+// #include <algorithm>
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to sort the array into a wave-like array.
+    void convertToWave(int n, vector<int>& arr){
+        
+        // Approach - )-- 1 Generalised Approach
+        // for(int i=1;i<n;i+=2){
+        //     if(arr[i]>arr[i-1]){
+        //         swap(arr[i],arr[i-1]);
+        //     }
+        //     if(arr[i]>arr[i+1] and i<=n-2){
+        //         swap(arr[i],arr[i+1]);
+        //     }
+        // }
+        
+        
+        // Approach - )-- 2 We can take the advantage of the sorted array
+        // for(int i=0;i<n-1;i+=2){
+        //     swap(arr[i],arr[i+1]);
+        // }
+        
+        
+        // Approach - )-- 3 We can take the advantage of the sorted array
+        for(int i=1;i<n;i+=2){
+            swap(arr[i],arr[i-1]);
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t; //Input testcases
+    while(t--) //While testcases exist
+    {
+        cin>>n; //input size of array
+        vector<int> a(n); //declare vector of size n
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //input elements of array
+        sort(a.begin(),a.end());
+        Solution ob;
+        ob.convertToWave(n, a);
+
+        for(int i=0;i<n;i++)
+            cout<<a[i]<<" "; //print array
+            
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
