df578dac445ed14acccf537be2c045db7780131d
Rahul Gupta <115313128+RahulGupta403@users.noreply.github.com>
2023-10-23 22:40:21 +0530

    Created Maximum_Sum_Increasing_Subsequence.cpp

---
+++ Maximum_Sum_Increasing_Subsequence.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+
+	public:
+	int maxSumIS(int arr[], int n)  
+	{  
+	    // Your code goes here
+	    vector<int> dp(arr, arr + n);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < i; j++)
+                if (arr[j] < arr[i])
+                    dp[i] = max(dp[i], dp[j] + arr[i]);
+        return *max_element(dp.begin(), dp.end());
+	}  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+      
+
+	    Solution ob;
+	    cout << ob.maxSumIS(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
