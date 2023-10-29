1f19703822589545a1edb3d92a1b59479e5692de
Rahul Gupta <115313128+RahulGupta403@users.noreply.github.com>
2023-10-29 23:37:09 +0530

    Created Check_Whether_K-th_bit_is_set_or_not.cpp

---
+++ Check_Whether_K-th_bit_is_set_or_not.cpp
@@ -0,0 +1,17 @@
+//User function Template for C++
+
+
+class Solution
+{
+    public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k)
+    {
+        // Your code here
+        // It can be a one liner logic!! Think of it!!
+    return (n & (1<<(k)));
+
+   
+    }
+    
+};
