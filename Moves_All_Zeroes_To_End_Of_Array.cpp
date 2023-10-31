d3eeec7b95ae51fe1ea415421505b2b577337077
Rahul Gupta <115313128+RahulGupta403@users.noreply.github.com>
2023-10-31 19:31:15 +0530

    Created Moves_All_Zeroes_To_End_Of_Array.cpp

---
+++ Moves_All_Zeroes_To_End_Of_Array.cpp
@@ -0,0 +1,11 @@
+class Solution{
+public:
+	void pushZerosToEnd(int arr[], int n) {
+	    int j=0;
+	    for(int i=0;i<n;i++){
+	        if(arr[i]!=0){
+	            swap(arr[i],arr[j++]);
+	        }
+	    }
+	}
+};
