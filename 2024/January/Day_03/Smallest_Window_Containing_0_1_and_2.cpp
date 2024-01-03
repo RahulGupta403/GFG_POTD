25ceeced1ccaf16ac4af1e592c488c338e9f4939
Rahul Gupta <115313128+RahulGupta403@users.noreply.github.com>
2024-01-03 16:10:10 +0530

    Created Smallest_Window_Containing_0_1_and_2.cpp

---
+++ 2024/January/Day_03/Smallest_Window_Containing_0_1_and_2.cpp
@@ -0,0 +1,44 @@
+class Solution {
+  public:
+    int smallestSubstring(string s)
+    {
+        int n=s.size();
+        int zeroCount=0;
+        int oneCount=0;
+        int twoCount=0;
+        int size=INT_MAX;
+        int i=0;
+        int j=0;
+        while(j<n){
+          if(s[j]=='0'){
+              zeroCount++;
+          }
+          else if(s[j]=='1'){
+              oneCount++;
+          }
+          else if(s[j]=='2'){
+              twoCount++;
+          }
+          if(zeroCount>=1 and oneCount>=1 and 
+          twoCount>=1){
+            while(zeroCount>=1 and oneCount>=1 and 
+          twoCount>=1){
+              size=min(size,j-i+1);
+              if(s[i]=='0'){
+                  zeroCount--;
+              }
+              else if(s[i]=='1'){
+                  oneCount--;
+              }
+              else if(s[i]=='2'){
+                  twoCount--;
+              }
+              i++;
+            }
+          }
+          j++;
+        }
+    if(size==INT_MAX) return -1;
+    return size;
+    }
+};
