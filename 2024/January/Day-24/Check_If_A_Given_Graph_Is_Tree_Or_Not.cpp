eab0987fd0f225475e4de2c987138cd8355507f5
Rahul Gupta <115313128+RahulGupta403@users.noreply.github.com>
2024-01-24 23:15:09 +0530

    Created Check_If_A_Given_Graph_Is_Tree_Or_Not.cpp

---
+++ 2024/January/Day-24/Check_If_A_Given_Graph_Is_Tree_Or_Not.cpp
@@ -0,0 +1,30 @@
+// Simple Approach maintain an adjacency list and do Depth FIrst Travesal and check if all nodes are visited or not.:-)
+class Solution {
+  public:
+    void dfs(vector<int>adj[],int node,vector<int>&visited){
+        visited[node]=1;
+        for(auto &it:adj[node]){
+            if(!visited[it]){
+                dfs(adj,it,visited);
+            }
+        }
+    }
+    int isTree(int n, int m, vector<vector<int>> &edges) {
+        if((n-m)!=1){
+            return 0;
+        }
+        vector<int> adj[n];
+        for(auto &it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> visited(n,0);
+        dfs(adj,0,visited);
+        for(auto &it:visited){
+            if(it==0){
+                return 0;
+            }
+        }
+    return 1;
+    }
+};
