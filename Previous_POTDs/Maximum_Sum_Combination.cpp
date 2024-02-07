34b4adedc8d4fb598a85f68e2a04d397435555d9
Rahul Gupta <115313128+RahulGupta403@users.noreply.github.com>
2024-02-08 04:35:06 +0530

    Updated Solution of Maximum_Sum_Combination.cpp

--- Previous_POTDs/Maximum_Sum_Combination.cpp
+++ Previous_POTDs/Maximum_Sum_Combination.cpp
@@ -1,53 +1,31 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    vector<int> maxCombinations(int n,int k,vector<int>&arr1,vector<int>&arr2){
-        sort(arr1.begin(),arr1.end(),greater<int>());
-        sort(arr2.begin(),arr2.end(),greater<int>());
-        priority_queue<pair<int,int>> maxheap;
+    typedef pair<int,int> pi;
+    typedef pair<int,pair<int,int>> ppi;
+    vector<int> maxCombinations(int n,int k,vector<int>&arr,vector<int> &brr){
         vector<int> ans;
-        for(int i=0;i<n;i++){
-            maxheap.push({(arr1[0]+arr2[i]),0});
-        }
+        set<pi> set;
+        priority_queue<ppi>pq;
+        sort(arr.begin(),arr.end());
+        sort(brr.begin(),brr.end());
+        pq.push({arr[n-1]+brr[n-1],{n-1,n-1}});
+        set.insert({n-1,n-1});
         while(k--){
-            auto x=maxheap.top();
-            int i=x.first;
-            int j=x.second;
-            maxheap.pop();
-            ans.push_back(i);
-            
-            maxheap.push({i-arr1[j]+arr1[j+1],j+1});
+            ppi temp=pq.top();
+            int sum=temp.first;
+            int x=temp.second.first;
+            int y=temp.second.second;
+            ans.push_back(sum);
+            pq.pop();
+            if(set.find({x-1,y})==set.end()){
+                pq.push({arr[x-1]+brr[y],{x-1,y}});
+                set.insert({x-1,y});
+            }
+            if(set.find({x,y-1})==set.end()){
+                pq.push({arr[x]+brr[y-1],{x,y-1}});
+                set.insert({x,y-1});
+            }
         }
     return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int N, K;
-        cin >> N >> K;
-
-        vector<int> A(N), B(N);
-        for (int i = 0; i < N; i++) {
-            cin >> A[i];
-        }
-        for (int i = 0; i < N; i++) {
-            cin >> B[i];
-        }
-        Solution obj;
-        vector<int> ans = obj.maxCombinations(N, K, A, B);
-        for (auto &it : ans) cout << it << ' ';
-        cout << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
