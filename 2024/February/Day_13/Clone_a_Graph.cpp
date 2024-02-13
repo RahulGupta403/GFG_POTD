2732d9fbcda2bad44c0eabceea6b657caf1e13d3
Rahul Gupta <115313128+RahulGupta403@users.noreply.github.com>
2024-02-13 23:29:00 +0530

    Updated Clone_a_Graph.cpp

--- 2024/February/Day_13/Clone_a_Graph.cpp
+++ 2024/February/Day_13/Clone_a_Graph.cpp
@@ -1,1 +1,32 @@
-D
+//User function Template for C++
+
+// struct Node {
+//     int val;
+//     vector<Node*> neighbors;
+//     Node() {
+//         val = 0;
+//         neighbors = vector<Node*>();
+//     }
+//     Node(int _val) {
+//         val = _val;
+//         neighbors = vector<Node*>();
+//     }
+//     Node(int _val, vector<Node*> _neighbors) {
+//         val = _val;
+//         neighbors = _neighbors;
+//     }
+// };
+
+class Solution {
+public:
+    Node* vis[10001] = {NULL};
+    Node* cloneGraph(Node* node) {
+    Node* graph = new Node(node->val);
+    vis[node->val] = graph;
+    for(auto it: node->neighbors){
+        if(!vis[it->val]) cloneGraph(it);
+        graph->neighbors.push_back(vis[it->val]);
+    }
+    return graph;
+}
+};
