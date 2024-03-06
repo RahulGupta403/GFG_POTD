b4e529115179f02464e74cf855e67c3e59cbd885
Rahul Gupta <115313128+RahulGupta403@users.noreply.github.com>
2024-03-06 23:29:45 +0530

    Created Rabin_Karp.cpp

---
+++ 2024/March/Day_6/Rabin_Karp.cpp
@@ -0,0 +1,30 @@
+#User function Template for python3
+
+class Solution:
+    def search(self, pattern, text):
+        n,m=len(text),len(pattern)
+        if n < m:
+            return -1
+        else:
+            res=[]
+            for i in range(n):
+                if text[i:i+m]==pattern:
+                    res+=[1+i]
+            return res
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input().strip()
+        patt = input().strip()
+        ob = Solution()
+        ans = ob.search(patt, s)
+        for value in ans:
+            print(value,end = ' ')
+        print()
+# } Driver Code End
